c23587699bdbe8678f48607f2a35095f7753fcd9
react#1507 <127757116+reactor-coding@users.noreply.github.com>
2023-06-18 21:46:42 +0900

    init

---
+++ assets/Illegal-reprint.js
@@ -0,0 +1,35 @@
+
+const Illegal_content = {
+  "scratch": [
+    {
+      name: "NIKOPON123",
+      url: "https://scratch.mit.edu/users/NIKOPON123/",
+      contents: [
+        {
+          date: "2023/06/10",
+          removed: true
+        }
+      ]
+    }, {
+      name: "abareru--------",
+      url: "https://scratch.mit.edu/users/abareru--------/",
+      contents: [
+        {
+					date: "2023/06/10",
+          url: "https://scratch.mit.edu/projects/852214145/"
+        }, {
+					date: "2023/06/10",
+					url: "https://scratch.mit.edu/projects/847171280/"
+				}
+      ]
+  	}
+  ]
+}
+
+let table;
+
+window.addEventListener("DOMContentLoaded", () => {
+  Illegal_content.scratch.forEach(content => {
+		
+	})
+})
